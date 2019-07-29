ddf8c1374237538061db23688c780576650bf543
filmscore88 <cmtmorales24@gmail.com>
2019-07-29 09:01:59 -0500

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,6 +1,22 @@
 export default function createStore(reducer) {
-  // add your code here
-}
+  let state;
+
+  function dispatch(action){
+    state= reducer(state, action);
+    render();
+  }
+
+  function getState(){
+    return state;
+  };
+
+  dispatch({ type: '@@INIT' })
+  
+  return {
+    getState,
+    dispatch
+  }
+};
 
 function render() {
   const container = document.getElementById('container');
